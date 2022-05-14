2835cfdd02ff530e088ea636da38127eec785633
MohamedYoussef <medyousef95@gmail.com>
2022-05-14 12:54:04 +0200

    added led

--- PIR.py
+++ PIR.py
@@ -9,7 +9,7 @@ GPIO.output(LED_1_PIN, GPIO.LOW)
 while True:
     time.sleep(0.1)
     print(GPIO.input(PIR_PIN))
-    if GPIO.input(PIR_PIN)==1:
+    if GPIO.input(PIR_PIN)==GPIO.HIGH:
         GPIO.output(LED_1_PIN, GPIO.HIGH)
     else:
         GPIO.output(LED_1_PIN, GPIO.LOW)
